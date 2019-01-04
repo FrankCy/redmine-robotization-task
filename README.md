ee17c5af87328b4acd71ccd171773a107c511442
cyoung <cy880708@163.com>
2019-01-04 10:41:01 +0800

    代码整理

--- README.md
+++ README.md
@@ -9,40 +9,39 @@
 注：通过此功能，集成Jenkins完成DevOps；
 ## 核心代码 ##
 ### 创建 ###
-```
+```java
 // Redmine管理器
 RedmineManager mgr = RedmineManagerFactory.createWithApiKey(uri, apiAccessKey);
-
 // 获取问题管理对象
 IssueManager issueManager = mgr.getIssueManager();
-
 // 设置跟踪 1：问题；2：功能
 Tracker tracker = TrackerFactory.create(1);
-
-Issue issueToCreate = IssueFactory.create(1, isEmpty(title)); //创建任务，并赋标题
-issueToCreate.setStatusId(1); //设置状态新建
-issueToCreate.setTracker(tracker); //设置跟踪
-issueToCreate.setAuthorId(1); //设置发起人
-issueToCreate.setAssigneeId(9); //设置指派人
-issueToCreate.setDescription(isEmpty(description)); //设置问题描述
-issueManager.createIssue(issueToCreate); //创建任务
-
+// 创建任务，并赋标题
+Issue issueToCreate = IssueFactory.create(1, isEmpty(title));
+// 设置状态新建
+issueToCreate.setStatusId(1);
+// 设置跟踪
+issueToCreate.setTracker(tracker);
+// 设置发起人
+issueToCreate.setAuthorId(1);
+// 设置指派人
+issueToCreate.setAssigneeId(9);
+// 设置问题描述
+issueToCreate.setDescription(isEmpty(description));
+// 创建任务
+issueManager.createIssue(issueToCreate);
 ```
 
 ### 删除 ###
-```
+```java
 // 目标项目
 String projectKey = "first_pro";
-
 // 声明Redmine管理器
 RedmineManager mgr = RedmineManagerFactory.createWithApiKey(uri, apiAccessKey);
-
 // 获取问题管理对象
 IssueManager issueManager = mgr.getIssueManager();
-
 // 获取所有问题
 List<Issue> issues = issueManager.getIssues(projectKey, null);
-
 // 迭代输出问题
 for (Issue issue : issues) {
     System.out.println(issue.getDescription());
@@ -54,5 +53,17 @@ for (Issue issue : issues) {
 ### 地址 ###
 访问地址：
 http://localhost:8681/redmine/index
+<br/>
+
+用户：
+http://192.168.10.110:3000/users.xml
+<br/>
+
+任务状态：
+http://192.168.10.110:3000/issue_statuses.xml
+<br/>
+
+任务：
+http://192.168.10.110:3000/issues.xml
 <br/>
 
--- src/main/java/com/redmine/rz/issue/RedmineIssueManager.java
+++ src/main/java/com/redmine/rz/issue/RedmineIssueManager.java
@@ -46,26 +46,23 @@ public class RedmineIssueManager {
 
         // Redmine管理器
         RedmineManager mgr = RedmineManagerFactory.createWithApiKey(uri, apiAccessKey);
-
         // 获取问题管理对象
         IssueManager issueManager = mgr.getIssueManager();
-
         // 设置跟踪 1：问题；2：功能
         Tracker tracker = TrackerFactory.create(1);
-
-        //创建任务，并赋标题
+        // 创建任务，并赋标题
         Issue issueToCreate = IssueFactory.create(1, isEmpty(title));
-        //设置状态新建
+        // 设置状态新建
         issueToCreate.setStatusId(1);
-        //设置跟踪
+        // 设置跟踪
         issueToCreate.setTracker(tracker);
-        //设置发起人
+        // 设置发起人
         issueToCreate.setAuthorId(1);
-        //设置指派人
+        // 设置指派人
         issueToCreate.setAssigneeId(9);
-        //设置问题描述
+        // 设置问题描述
         issueToCreate.setDescription(isEmpty(description));
-        //创建任务
+        // 创建任务
         issueManager.createIssue(issueToCreate);
 
         return true;
@@ -84,13 +81,10 @@ public class RedmineIssueManager {
 
         // Redmine管理器
         RedmineManager mgr = RedmineManagerFactory.createWithApiKey(uri, apiAccessKey);
-
         // 获取问题管理对象
         IssueManager issueManager = mgr.getIssueManager();
-
         // 设置跟踪 1：问题；2：功能
         Tracker tracker = TrackerFactory.create(2);
-
         Issue issueToCreate = IssueFactory.create(1, isEmpty(issueBean.getTitle()));
         // 状态
         issueToCreate.setStatusId(1);
@@ -322,16 +316,12 @@ public class RedmineIssueManager {
     public static void deleteIssues() throws Exception {
         // 目标项目
         String projectKey = "first_pro";
-
         // 声明Redmine管理器
         RedmineManager mgr = RedmineManagerFactory.createWithApiKey(uri, apiAccessKey);
-
         // 获取问题管理对象
         IssueManager issueManager = mgr.getIssueManager();
-
         // 获取所有问题
         List<Issue> issues = issueManager.getIssues(projectKey, null);
-
         // 迭代输出问题
         for (Issue issue : issues) {
             System.out.println(issue.getDescription());
